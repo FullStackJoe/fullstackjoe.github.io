eb3bbee94ad262be4b5db4b3c36531c636723126
Johan Nielsen <johanhn10@gmail.com>
2023-04-05 19:34:32 +0200

    hell adø äsfæ åsdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+import Pokedex from "./pages/Pokedex";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Pokedex />} />
+
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
